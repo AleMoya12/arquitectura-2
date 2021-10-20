ca38a2130c715180c3f6984e2c227b28c2926003
Ezequiel Ramirez <ezequielram@gmail.com>
2021-10-20 16:23:22 -0300

    ajuste textos

---
+++ correo-contacto.php
@@ -0,0 +1,10 @@
+<?php
+    $destino= "beatriz.fidel@gmail.com";
+    $nombre = $_POST["nombre"];
+    $apellido = $_POST["apellido"];
+    $correo = $_POST["correo"];
+    $mensaje = $_POST["messaje"];
+    $contenido = "Nombre: " . $nombre . "\nApellido: " . $apellido . "\nCorreo: " . $correo . "\nMensaje: " . $mensaje . "\nDesea Info sobre tus servicios ";
+    mail($destino, "Nueva consulta desde Sitio Web", $contenido);
+    header("Location:index.html");
+?>
